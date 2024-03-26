afa133eb00004949ea73e82693f6b2a328c3320b
Andrew Drogalis <108765079+andrew-drogalis@users.noreply.github.com>
2024-03-26 15:05:22 -0400

    Folder organization

--- test/unit_test.cpp
+++ test/unit_test.cpp
@@ -1,1 +1,19 @@
+// Copyright 2024, Andrew Drogalis
+// GNU License
 
+#include "gain_capital_api.h"
+
+#include "gtest/gtest.h"
+
+namespace {
+
+TEST(GainCapital, DefaultConstructor) {
+    const gaincapital::GCapiClient g;
+    
+    EXPECT_EQ(g.trading_account_id, "");
+    EXPECT_EQ(g.client_account_id, "");
+}
+
+
+
+} // namespace
